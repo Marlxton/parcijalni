708e61a19f7857c0eed7c9f3bd1996f4ab437238
Marlxton <skendertoni2@gmail.com>
2022-07-14 14:12:20 +0200

    style

--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -1,7 +1,7 @@
 const Repositories = ({ repos }) => {
     return (
       <>
-        <h3>Repositories</h3>
+        <h3>Repositories:</h3>
         <ul>
           {repos.map((repo) => {
             return <li key={repo.id}>{repo.name}</li>;
